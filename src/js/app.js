7561b7d4c27cb4f8fc44361d86b2d9fd8a5cbb45
ptrv-dev <petrov99119@gmail.com>
2023-03-13 21:57:31 +0200

    Header - Responsive all devices

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,23 @@
+const headerMenuButton = document.querySelector('.header__menu');
+const headerMenuNavigation = document.querySelector('nav.header__nav.nav');
+const headerMenuActions = document.querySelector('.header__actions');
+const headerContainer = document.querySelector('.header__container');
+
+headerMenuButton.addEventListener('click', () => {
+  headerMenuButton.classList.toggle('header__menu_active');
+  headerMenuNavigation.classList.toggle('nav_active');
+});
+
 const handleResize = () => {
   const w = window.innerWidth;
   console.log(w);
 
   if (w <= 1460) {
+  }
+  if (w <= 768) {
+    headerMenuNavigation.append(headerMenuActions);
+  } else {
+    headerContainer.insertBefore(headerMenuActions, headerMenuButton);
   }
 };
 
